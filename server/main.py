e39a31dce54e132db1936095fa0fb84ebb7df58d
hdariodev <hdariodev@gmail.com>
2024-05-25 21:18:00 -0400

    test fastAPI

--- server/main.py
+++ server/main.py
@@ -1,4 +1,21 @@
 import store
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [1,2,3,4,5]
+
+@app.get("/products")
+def get_data():
+    return [{
+        "id": 1,
+        "title": "Playera",
+        "price": 25,
+        "description": "bla bla bla",
+        "image": "https://www.google.com"
+    }]
 
 def run():
     store.get_products()
